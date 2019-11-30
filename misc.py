34550d24e79809044a3bd0a75bfd0a9dce479ea3
Matěj Račinský <racinsky.matej@seznam.cz>
2019-11-30 22:22:20 +0100

    new year after successful testing

--- misc.py
+++ misc.py
@@ -9,7 +9,7 @@ def read_day(i: int):
     with open(f'day_{i}/input.txt', 'r', encoding='utf-8') as f:
         in_data = f.read()
     if in_data == '':
-        in_data = get_data(token, day=i, year=2018)
+        in_data = get_data(token, day=i, year=2019)
         with open(f'day_{i}/input.txt', 'w', encoding='utf-8') as f:
             f.write(in_data)
     return in_data
