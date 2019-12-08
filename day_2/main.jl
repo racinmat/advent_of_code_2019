530f0d88f0f356af2b737140b5abaa8000fd2dad
Matěj Račinský <racinsky.matej@seznam.cz>
2019-12-08 22:45:56 +0100

    done day 7, done day 8, rewritten intmachine for days 2, 5 and 7

--- day_2/main.jl
+++ day_2/main.jl
@@ -1,42 +1,26 @@
 using DrWatson
 quickactivate(@__DIR__)
 include(projectdir("misc.jl"))
+include(projectdir("interpret.jl"))
 
 cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 data = cur_day |> read_input |> x->read_numbers(x, ',')
 # data = cur_day |> test_input |> x->read_numbers(x, ',')
 
-function run_program(arr)
-    i = 1
-    while arr[i] != 99
-        if arr[i] == 1
-            arr[arr[i+3]+1] = arr[arr[i+1]+1] + arr[arr[i+2]+1]
-        elseif arr[i] == 2
-            arr[arr[i+3]+1] = arr[arr[i+1]+1] * arr[arr[i+2]+1]
-        else
-            throw("fuck it")
-        end
-        i += 4
-    end
-    arr[1]
-end
-
 function part1()
     arr = copy(data)
     arr[2] = 12
     arr[3] = 2
-    run_program(arr)
+    run_program!(arr)
+    arr[1]
 end
 
 function part2()
     for (i, j) in Iterators.product(0:99, 0:99)
         arr = copy(data)
-        arr[2] = i
-        arr[3] = j
-        res = run_program(arr)
-        if res == 19690720
-            return 100 * arr[2] + arr[3]
-        end
+        arr[2], arr[3] = i, j
+        run_program!(arr)
+        arr[1] == 19690720 && return 100 * arr[2] + arr[3]
     end
 end
 
--- day_8/main.jl
+++ day_8/main.jl
@@ -1,19 +1,23 @@
 using DrWatson
 quickactivate(@__DIR__)
+using Images
 include(projectdir("misc.jl"))
 
 cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
-data = read_input(cur_day)
+data = read_input(cur_day) |> x->split(x, "") |> x->parse.(Int, x) |> x->reshape(x, (25, 6, :))
 
 function part1()
-    data
+    mxval, mxindx = findmin(sum(data .== 0, dims=[1, 2]))
+    sum(data[:, :, mxindx[3]] .== 1) * sum(data[:, :, mxindx[3]] .== 2)
 end
 
 function part2()
-    data
+    mxval, mxindx = findmax(data .< 2, dims=3)
+    res_matrix = data[mxindx]
+    save("day-8-img.png", colorview(Gray, convert.(Float64, res_matrix[:, :, 1])))
 end
 
 println(part1())
-submit(part1(), cur_day, 1)
+# submit(part1(), cur_day, 1)
 println(part2())
 submit(part2(), cur_day, 2)
