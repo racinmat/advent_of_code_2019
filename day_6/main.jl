f1deadafcf31f6eacc67ec14e3f1bab22f22d9d1
Račinský Matěj <matej.racinsky@avast.com>
2019-12-09 09:37:44 +0100

    done day 6

--- day_6/main.jl
+++ day_6/main.jl
@@ -2,18 +2,46 @@ using DrWatson
 quickactivate(@__DIR__)
 include(projectdir("misc.jl"))
 
+using LightGraphs, MetaGraphs
+using Base.Iterators: flatten
+
 cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
-data = read_input(cur_day)
+split_r = ")"
+strip_r(x) = replace(x, "\r"=>"")
+data = cur_day |> read_input |> strip_r |> rstrip |> x->split(x, '\n') .|> x->split(x, split_r)
+# data = cur_day |> test_input |> strip_r |> rstrip |> x->split(x, '\n') .|> x->split(x, split_r)
+
 
 function part1()
-    data
+    nodes = data |> flatten |> unique
+    g = DiGraph(length(nodes))
+    int2name = nodes |> enumerate |> Dict
+    name2int = Dict(v=>k for (k, v) in int2name)
+    for (node1, node2) in data
+        add_edge!(g, name2int[node1], name2int[node2])
+    end
+    toposorted = topological_sort_by_dfs(g)
+    states = floyd_warshall_shortest_paths(g)
+    tot_paths = 0
+    for i in g |> vertices
+        tot_paths += enumerate_paths(states, i) |> x->filter(y->length(y) > 0, x) |> length
+    end
+    tot_paths
 end
 
 function part2()
-    data
+    nodes = data |> flatten |> unique
+    g = Graph(length(nodes))
+    int2name = nodes |> enumerate |> Dict
+    name2int = Dict(v=>k for (k, v) in int2name)
+    for (node1, node2) in data
+        add_edge!(g, name2int[node1], name2int[node2])
+    end
+    paths = dijkstra_shortest_paths(g, name2int["YOU"])
+    paths.dists[paths.parents[name2int["SAN"]]] - 1
 end
 
 println(part1())
-submit(part1(), cur_day, 1)
+# submit(part1(), cur_day, 1)
 println(part2())
-submit(part2(), cur_day, 2)
+# submit(part2(), cur_day, 2)
