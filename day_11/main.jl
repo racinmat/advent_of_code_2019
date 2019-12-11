8bc7c96b75025f6d2e1b58484ee04f4fff7c4045
Matěj Račinský <racinsky.matej@seznam.cz>
2019-12-11 22:30:27 +0100

    day 11 part 1 done

--- day_11/main.jl
+++ day_11/main.jl
@@ -1,12 +1,58 @@
 using DrWatson
 quickactivate(@__DIR__)
 include(projectdir("misc.jl"))
+include(projectdir("interpret.jl"))
+
+abstract type Direction end
+struct Up <: Direction end
+struct Down <: Direction end
+struct Left <: Direction end
+struct Right <: Direction end
+
+rot_left(dir::Type{Up}) = Left
+rot_left(dir::Type{Down}) = Right
+rot_left(dir::Type{Left}) = Down
+rot_left(dir::Type{Right}) = Up
+
+rot_right(dir::Type{Up}) = Right
+rot_right(dir::Type{Down}) = Left
+rot_right(dir::Type{Left}) = Up
+rot_right(dir::Type{Right}) = Down
+
+rot(dir, i) = i == 0 ? rot_left(dir) : rot_right(dir)
+
+move(i, dir::Type{Up}) = i + CartesianIndex(-1, 0)
+move(i, dir::Type{Down}) = i + CartesianIndex(1, 0)
+move(i, dir::Type{Left}) = i + CartesianIndex(0, -1)
+move(i, dir::Type{Right}) = i + CartesianIndex(0, 1)
 
 cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
-data = read_input(cur_day)
+data = cur_day |> read_input |> x->read_numbers(x, ',')
 
 function part1()
-    data
+    visited = Set{CartesianIndex}()
+    grid = zeros(Int, 10000, 10000)
+    point = CartesianIndex(size(grid) .÷ 2)
+    direction = Up
+    channel_in = Channel(Inf)
+    channel_out = Channel(Inf)
+    program = @async run_program!(copy(data), channel_in, channel_out)
+    push!(visited, point)
+    should_stop = false
+    while !should_stop
+        put!(channel_in, grid[point])
+        if istaskdone(channel_out)
+            should_stop = true
+            break
+        end
+        color = take!(channel_out)
+        grid[point] = color
+        rotation = take!(channel_out)
+        direction = rot(direction, rotation)
+        point = move(point, direction)
+        push!(visited, point)
+    end
+    length(visited)
 end
 
 function part2()
