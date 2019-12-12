c15b6546e2fea1116effca5299b99328599c334f
Račinský Matěj <matej.racinsky@avast.com>
2019-12-12 11:25:15 +0100

    done day 12 part 1

--- day_12/main.jl
+++ day_12/main.jl
@@ -1,19 +1,43 @@
 using DrWatson
 quickactivate(@__DIR__)
+using Combinatorics, StaticArrays
+using Base.Iterators: flatten
 include(projectdir("misc.jl"))
 
 cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
-data = read_input(cur_day)
+
+function parse_row(str)
+    m = match(r"<x=(-?[0-9]+), y=(-?[0-9]+), z=(-?[0-9]+)>", str)
+    Vector(parse.(Int, [m[1], m[2], m[3]]))
+end
+
+const data = cur_day |> read_input |> x->split(x, '\n') .|> parse_row
+# const data = cur_day |> test_input |> x->rstrip(x, '\n') |> x->split(x, '\n') .|> parse_row
+
+function iteration!(positions, velocities)
+    for (idx1, idx2) in combinations(1:size(positions)[1], 2)
+        @inbounds velocities[idx1, :] += sign.(positions[idx2, :] - positions[idx1, :])
+        @inbounds velocities[idx2, :] += sign.(positions[idx1, :] - positions[idx2, :])
+    end
+    positions .+= velocities
+end
 
 function part1()
-    data
+    positions = MMatrix{4, 3}(copy(hcat(data...)'))
+    velocities = MMatrix{4, 3}(zeros(Int, size(positions)))
+    for i in 1:1000
+        iteration!(positions, velocities)
+    end
+    sum(sum(abs.(positions), dims=2) .* sum(abs.(velocities), dims=2))
 end
 
 function part2()
     data
 end
 
+using BenchmarkTools
 println(part1())
-submit(part1(), cur_day, 1)
+@btime part1()
+# submit(part1(), cur_day, 1)
 println(part2())
 submit(part2(), cur_day, 2)
