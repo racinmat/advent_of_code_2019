f4f99d90bdcef73974cb73f176a61a24cc2e0286
Matěj Račinský <racinsky.matej@seznam.cz>
2019-12-13 22:49:09 +0100

    added numpy + numba solution

--- day_12/main.jl
+++ day_12/main.jl
@@ -14,8 +14,9 @@ end
 const data = cur_day |> read_input |> x->split(x, '\n') .|> parse_row
 # const data = cur_day |> test_input |> x->rstrip(x, '\n') |> x->split(x, '\n') .|> parse_row
 
-function iteration!(positions, velocities)
-    for (idx1, idx2) in combinations(1:size(positions)[1], 2)
+function iteration!(positions, velocities, combs)
+    @simd for i in 1:length(combs)
+        idx1, idx2 = combs[i]
         @inbounds velocities[idx1, :] += sign.(positions[idx2, :] - positions[idx1, :])
         @inbounds velocities[idx2, :] += sign.(positions[idx1, :] - positions[idx2, :])
     end
@@ -25,8 +26,9 @@ end
 function part1()
     positions = MMatrix{4, 3}(copy(hcat(data...)'))
     velocities = MMatrix{4, 3}(zeros(Int, size(positions)))
+    combs = combinations(1:size(positions)[1], 2) |> collect
     for i in 1:1000
-        iteration!(positions, velocities)
+        iteration!(positions, velocities, combs)
     end
     sum(sum(abs.(positions), dims=2) .* sum(abs.(velocities), dims=2))
 end
@@ -36,6 +38,7 @@ function part2()
 end
 
 using BenchmarkTools
+
 println(part1())
 @btime part1()
 # submit(part1(), cur_day, 1)
---
+++ day_12/main.py
@@ -0,0 +1,43 @@
+import itertools
+import re
+import timeit
+
+import numpy as np
+from numba import jit
+
+import misc
+
+
+def parse_row(str):
+    m = re.match(r"<x=(-?[0-9]+), y=(-?[0-9]+), z=(-?[0-9]+)>", str)
+    return np.array([int(m.group(1)), int(m.group(2)), int(m.group(3))])
+
+
+data = [parse_row(i) for i in misc.read_day(12).split('\n')]
+
+
+@jit(nopython=True)
+def iteration(positions, velocities, combs):
+    for (idx1, idx2) in combs:
+        velocities[idx1, :] += np.sign(positions[idx2, :] - positions[idx1, :])
+        velocities[idx2, :] += np.sign(positions[idx1, :] - positions[idx2, :])
+
+    positions += velocities
+
+
+def part1():
+    positions = np.array(data)
+    velocities = np.zeros_like(positions)
+    combs = list(itertools.combinations(range(len(positions)), 2))
+    for i in range(1000):
+        iteration(positions, velocities, combs)
+
+    return sum(np.sum(np.abs(positions), axis=1) * np.sum(np.abs(velocities), axis=1))
+
+
+print(part1())
+print(timeit.timeit(lambda: part1(), number=1000))
+# @btime part1()
+# submit(part1(), cur_day, 1)
+# println(part2())
+# submit(part2(), cur_day, 2)
