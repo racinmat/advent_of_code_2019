b2111fa02bf4eda4a6aa57fb2757f11f582d2cf0
Matěj Račinský <racinsky.matej@seznam.cz>
2019-12-19 01:26:20 +0100

    day 17 part 1 refactored

--- day_17/main.jl
+++ day_17/main.jl
@@ -3,27 +3,46 @@ quickactivate(@__DIR__)
 include(projectdir("misc.jl"))
 include(projectdir("interpret.jl"))
 
-idx_directions = [CartesianIndex(-1, 0), CartesianIndex(1, 0), CartesianIndex(0, -1), CartesianIndex(0, 1)]
+idx_directions = [
+    CartesianIndex(-1, 0),
+    CartesianIndex(1, 0),
+    CartesianIndex(0, -1),
+    CartesianIndex(0, 1),
+    CartesianIndex(0, 0)
+]
+
+offset = CartesianIndex(1, 1)
 
 cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 data = cur_day |> read_input |> x->read_numbers(x, ',')
 
-function part1()
+function build_grid(data)
     grid_line = run_program_all_out!(copy(data)) .|> Char
     grid_lines = Vector{Vector{Char}}()
 
     while length(grid_line) > 1
         line_end = findfirst(x->x=='\n', grid_line)
         push!(grid_lines, grid_line[1:line_end-1])
-        global grid_line = grid_line[line_end+1:end]
+        grid_line = grid_line[line_end+1:end]
     end
-    grid = hcat(grid_lines...)
-    crossings = Vector{CartesianIndex}()
-    for i in CartesianIndices(grid[2:end-1, 2:end-1])
-        if
-        crossing =
-        push!(crossings, crossing)
+    hcat(grid_lines...)
+end
+
+function part1()
+    grid = build_grid(data)
+    crossings = Vector{CartesianIndex{2}}()
+    for i in CartesianIndices(grid[1:end-2, 1:end-2])
+        if all('#' == grid[i + j + offset] for j in idx_directions)
+            push!(crossings, i + offset)
+        end
     end
+    grid[crossings] .= 'O'
+
+    res = 0
+    for i in crossings
+        res += (i[1] - 1) * (i[2] - 1)
+    end
+    res
 end
 
 function part2()
