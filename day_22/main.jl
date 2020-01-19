7abc882f4a923e67cbb9ead5f3f393fa5cd4258c
Matěj Račinský <racinsky.matej@seznam.cz>
2020-01-19 23:16:00 +0100

    refactored solution, still slow

--- day_22/main.jl
+++ day_22/main.jl
@@ -6,13 +6,6 @@ include(projectdir("misc.jl"))
 cur_day = parse(Int, splitdir(@__DIR__)[end][5:end])
 data = cur_day |> read_input |> x->split(x, '\n')
 
-cut_deck(card::Int, arr_len, i) = mod(card - i, arr_len)
-deal_new_stack(card::Int, arr_len) = arr_len - card - 1
-deal_increment(card::Int, arr_len, i) = mod(card * i, arr_len)
-process_instruction(card::Int, arr_len, i::CutInstruction) = mod(card - i.i, arr_len)
-process_instruction(card::Int, arr_len, i::DealNewInstruction) = arr_len - card - 1
-process_instruction(card::Int, arr_len, i::DealIncrementInstruction) = mod(card * i.i, arr_len)
-
 abstract type Instruction end
 struct CutInstruction <: Instruction
     i::Int
@@ -21,6 +14,14 @@ struct DealNewInstruction <: Instruction end
 struct DealIncrementInstruction <: Instruction
     i::Int
 end
+
+process_instruction(card::Int, arr_len, i::CutInstruction) = mod(card - i.i, arr_len)
+process_instruction(card::Int, arr_len, i::DealNewInstruction) = arr_len - card - 1
+process_instruction(card::Int, arr_len, i::DealIncrementInstruction) = mod(card * i.i, arr_len)
+
+process_instruction_rev(card::Int, arr_len, i::CutInstruction) = mod(card + i.i, arr_len)
+process_instruction_rev(card::Int, arr_len, i::DealNewInstruction) = arr_len - card - 1
+process_instruction_rev(card::Int, arr_len, i::DealIncrementInstruction) = mod(invmod(i.i, arr_len) * card, arr_len)
 
 function parse_instruction(row)
     if match(r"cut -?\d+", row) != nothing
@@ -35,50 +36,39 @@ function parse_instruction(row)
 end
 
 function part1()
-    deck = collect(0:10006)
-    for row in data
-        deck = process_instruction!(deck, row)
-    end
-    findfirst(x->x==2019, deck)-1
-end
-
-function part12()
-    card = 2019
-    for row in data
-        card = process_instruction(card, 10007, row)
+    card = 2_019
+    instructions = parse_instruction.(data)
+    for row in instructions
+        card = process_instruction(card, 10_007, row)
     end
     card
 end
 
-function part11()
-    deck = collect(0:10006)
-    for i in 1:1000
-        for row in data
-            deck = process_instruction!(deck, row)
+function part2()
+    card = 2_020
+    instructions = reverse(parse_instruction.(data))
+    for i in 1:101_741_582_076_661
+        for row in instructions
+            card = process_instruction_rev(card, 119_315_717_514_047, row)
         end
     end
-    findfirst(x->x==2019, deck)-1
+    card
 end
 
-function part112()
-    card = 2019
-    instructions = parse_instruction.(data)
-    for i in 1:1000
+function exec_instructions(instructions::Vector{Union{Instruction}})
+    card = 2_020
+    # for i in 1:101_741_582_076_661
+    for i in 1:76_661
         for row in instructions
-            card = process_instruction(card, 10007, row)
+            card = process_instruction_rev(card, 119_315_717_514_047, row)
         end
     end
     card
 end
 
 function part2()
-    deck = collect(0:119315717514046)
-    for i in 1:101741582076661
-        for row in data
-            deck = process_instruction(deck, row)
-        end
-    end
-    findfirst(x->x==2019, deck)-1
+    instructions = reverse(parse_instruction.(data))
+    exec_instructions(instructions)
 end
 
 using BenchmarkTools
@@ -88,19 +78,11 @@ part1()
 display(to)
 
 to = TimerOutput()
-part12()
-display(to)
-
-to = TimerOutput()
-part11()
-display(to)
-
-to = TimerOutput()
-part112()
+part2()
 display(to)
 
 @btime part1()
-@btime part12()
+@btime part2()
 submit(part1(), cur_day, 1)
 println(part2())
 submit(part2(), cur_day, 2)
